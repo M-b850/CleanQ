4f59c03a31d693e669f07717c956a2bdd7356b88
Mehrshad <m-b850@protonmail.com>
2021-04-13 20:27:51 +0430

    Admin fixed for custom user model.

--- core/models.py
+++ core/models.py
@@ -20,6 +20,7 @@ class UserManager(BaseUserManager):
         user = self.create_user(email, password)
         user.is_staff = True
         user.is_superuser = True
+        user.is_active = True
         user.save(using=self._db)
 
         return user
@@ -32,6 +33,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     last_name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=False)
     is_staff = models.BooleanField(default=False)
+    is_clinic = models.BooleanField(default=False)
 
     objects = UserManager()
 
